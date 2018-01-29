6dc5aa01a73916da77d58af6eb1dbb85dbfbaa71
IbrahimG88 <goharyibrahim@gmail.com>
2018-01-29 22:45:22 +0200

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class Home {
+
+  tab1Root: any = 'Groceries';
+  tab2Root: any = 'Notes';
+  tab3Root: any = 'Contacts';
+  tab4Root: any = 'Ladder';
+
 
   constructor(public navCtrl: NavController) {
+
 
   }
 
